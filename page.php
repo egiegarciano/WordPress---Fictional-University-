fd35fd338e3b6a993fcac70bd867d7f3c2617ef9
egiegarciano <egiegarciano14@gmail.com>
2021-01-19 23:35:26 +0800

    building blog section

--- page.php
+++ page.php
@@ -37,7 +37,7 @@
 
     if ($theParent or $testArray) { ?>
     <div class="page-links">
-      <h2 class="page-links__title"><a href="<?php get_permalink($theParent); ?>"><?php echo get_the_title($theParent)?></a></h2>
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent)?></a></h2>
       <ul class="min-list">
         <?php 
           if ($theParent) {
